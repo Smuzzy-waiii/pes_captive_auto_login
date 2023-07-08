0ff076cb4a2937165f0773ff1c06fbed8b7d9fa6
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2023-07-08 12:09:10 +0530

    bug fix: ConnectionError used to crash the app. fixed now

--- curpl.py
+++ curpl.py
@@ -1,13 +1,19 @@
 import requests
-conn_err = requests.exceptions.ConnectionError
 
 def is_internet_enabled():
+	is_enabled = True
+
 	try:
 		r = requests.get("http://google.com/")
 	except requests.exceptions.ConnectionError as e:
+		is_enabled = False
 		r = e
-	is_enabled = (r != conn_err) and (r.status_code == 200) and ("192.168.254.1:8090" not in r.text)
+
+	is_enabled = is_enabled and (r.status_code == 200) and ("192.168.254.1:8090" not in r.text)
 	if not is_enabled:
-		print(f"Not able to reach internet. Response:", r)
+		print(f"Not able to reach internet. Response:", type(r), r)
 	return is_enabled
 	# returns true if google is successfully reachable
+
+if __name__=="__main__":
+	is_internet_enabled()
